ec9b9afc2203ec6df9de0a71a9c757284ae66050
Richard Bultitde <rbultitude@sapient.com>
2019-11-01 11:27:45 +0000

    output result of random index to DOM

--- js/app.mjs
+++ js/app.mjs
@@ -2,6 +2,7 @@ import randomIndex from './random-index.mjs';
 import randomArray from './random-array.mjs';
 
 const strArr = ['Could', 'be', 'anything', 'in', 'this', 'array'];
+const outputNode = document.getElementById('output');
 
-console.log('random array index is: ', randomIndex(strArr));
+outputNode.textContent = randomIndex(strArr);
 console.log('random array of numbers is: ', randomArray());
