aab5e44dd0d65c4cc5c653f0e0487a936408aa4e
Richard Bultitde <rbultitude@sapient.com>
2019-11-05 18:43:09 +0000

    add basic styles

--- js/app.mjs
+++ js/app.mjs
@@ -1,9 +1,8 @@
 import randomIndex from './random-index.mjs';
-import randomArray from './random-array.mjs';
+// import randomArray from './random-array.mjs';
 
 const outputEl = document.getElementById('output');
 const strArr = ['Could', 'be', 'anything', 'in', 'this', 'array'];
-const outputNode = document.getElementById('output');
 
-// outputEl.textContent = randomIndex(strArr);
-outputEl.textContent = `${randomArray()}`;
+outputEl.textContent = randomIndex(strArr);
+// outputEl.textContent = `${randomArray()}`;
