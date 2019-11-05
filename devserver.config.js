1f27e9c64286a8236cece2370908e2daebb3d11c
rjbultitude <richard.bultitude@gmail.com>
2019-11-05 22:40:10 +0000

    add web_modules (for Pika) to git ignore

--- devserver.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    mockPath: 'mock',
-    app: '/index.html', // this can also be an express app
-    serverConfig: {}, // config for webpack-hot-server-middleware
-    port: 5450, // 3000 by default
-};
