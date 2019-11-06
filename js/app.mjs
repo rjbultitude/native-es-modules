d5960905fdb986d5890e68df416ee1232c46f302
rjbultitude <richard.bultitude@gmail.com>
2019-11-06 09:54:48 +0000

    add dynamic import example

--- js/app.mjs
+++ js/app.mjs
@@ -1,6 +1,11 @@
 import randomIndex from './random-index.mjs';
 // import randomArray from './random-array.mjs';
 
+import('./random-index.mjs').then((module) => {
+    const randomIndex = module.default;
+    console.log('randomIndex', randomIndex(['0', '1']));
+});
+
 const outputEl = document.getElementById('output');
 const strArr = ['Could', 'be', 'anything', 'in', 'this', 'array'];
 
